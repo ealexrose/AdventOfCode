a7763188524791fbe2d8fd25910be3bd926055c2
ealexrose <47873095+ealexrose@users.noreply.github.com>
2024-12-02 23:46:01 -0600

    Day 3 Solution

--- Services/2024/Solution2024_03Service.cs
+++ Services/2024/Solution2024_03Service.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Services
 {
     // (ctrl/command + click) the link to open the input file
@@ -9,10 +11,19 @@ namespace AdventOfCode.Services
             List<string> lines = Utility.GetInputLines(2024, 3, example);
 
             int answer = 0;
+            string mulCapturePattern = "(mul\\(([0-9]{1,3}),([0-9]{1,3})\\))";
+            Regex mulCapture = new Regex(mulCapturePattern);
 
             foreach (string line in lines)
             {
-
+                foreach (Match multInstruction in Regex.Matches(line, mulCapturePattern, RegexOptions.None)) 
+                {
+                    string matchedString = multInstruction.Value;
+                    string[] instructionParts = matchedString.Split(',');
+                    int firstNumber = int.Parse(instructionParts[0].Substring(4, instructionParts[0].Length - 4));
+                    int secondNumber = int.Parse(instructionParts[1].Substring(0, instructionParts[1].Length - 1));
+                    answer += (firstNumber * secondNumber);
+                }
             }
 
             return answer.ToString();
@@ -23,10 +34,33 @@ namespace AdventOfCode.Services
             List<string> lines = Utility.GetInputLines(2024, 3, example);
 
             int answer = 0;
+            string mulCapturePattern = "(mul\\(([0-9]{1,3}),([0-9]{1,3})\\))|(do\\(\\))|(don't\\(\\))";
+            Regex mulCapture = new Regex(mulCapturePattern);
 
+            bool doing = true;
             foreach (string line in lines)
             {
+                foreach (Match multInstruction in Regex.Matches(line, mulCapturePattern, RegexOptions.None))
+                {
+                    if (multInstruction.Value == "do()")
+                    {
+                        doing = true;
+                        continue;
+                    }
+                    if (multInstruction.Value == "don't()") 
+                    {
+                        doing = false;
+                        continue;
+                    }
 
+                    if(!doing)
+                        continue;
+                    string matchedString = multInstruction.Value;
+                    string[] instructionParts = matchedString.Split(',');
+                    int firstNumber = int.Parse(instructionParts[0].Substring(4, instructionParts[0].Length - 4));
+                    int secondNumber = int.Parse(instructionParts[1].Substring(0, instructionParts[1].Length - 1));
+                    answer += (firstNumber * secondNumber);
+                }
             }
 
             return answer.ToString();
