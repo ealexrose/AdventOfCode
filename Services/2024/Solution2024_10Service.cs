d6e88b3bb23c3d5fca679c914bc064ab38d6deb8
ealexrose <47873095+ealexrose@users.noreply.github.com>
2024-12-14 20:04:28 -0600

    Day 10 solution

--- Services/2024/Solution2024_10Service.cs
+++ Services/2024/Solution2024_10Service.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Components;
+
 namespace AdventOfCode.Services
 {
     // (ctrl/command + click) the link to open the input file
@@ -9,10 +11,27 @@ namespace AdventOfCode.Services
             List<string> lines = Utility.GetInputLines(2024, 10, example);
 
             int answer = 0;
+            Trail[,] trails = new Trail[lines.Count, lines[0].Length];
+            List<(int x, int y)> trailHeads = new List<(int x, int y)>();
 
-            foreach (string line in lines)
+            int trailIndex = 0;
+            for (int i = 0; i < trails.GetLength(0); i++) 
             {
+                for (int j = 0; j < trails.GetLength(1); j++)
+                {
+                    int height = lines[i][j].ToInt();
+                    trails[i, j] = new Trail(height, trailIndex);
+                    if (height == 0)
+                    {
+                        trailIndex++;
+                        trailHeads.Add((i, j));
+                    }
 
+                }
+            }
+            foreach ((int x, int y) trailHead in trailHeads)
+            {
+                answer += CountDistinctTrailsFromTrailHead(ref trails, trailHead, trails[trailHead.x, trailHead.y].trailHeadId, 0, new List<(int x, int y)>(), false);
             }
 
             return answer.ToString();
@@ -23,13 +42,82 @@ namespace AdventOfCode.Services
             List<string> lines = Utility.GetInputLines(2024, 10, example);
 
             int answer = 0;
+            Trail[,] trails = new Trail[lines.Count, lines[0].Length];
+            List<(int x, int y)> trailHeads = new List<(int x, int y)>();
 
-            foreach (string line in lines)
+            int trailIndex = 0;
+            for (int i = 0; i < trails.GetLength(0); i++)
             {
+                for (int j = 0; j < trails.GetLength(1); j++)
+                {
+                    int height = lines[i][j].ToInt();
+                    trails[i, j] = new Trail(height, trailIndex);
+                    if (height == 0)
+                    {
+                        trailIndex++;
+                        trailHeads.Add((i, j));
+                    }
 
+                }
+            }
+            foreach ((int x, int y) trailHead in trailHeads)
+            {
+                answer += CountDistinctTrailsFromTrailHead(ref trails, trailHead, trails[trailHead.x, trailHead.y].trailHeadId, 0, new List<(int x, int y)>(), false);
             }
 
             return answer.ToString();
+        }
+
+
+        public int CountDistinctTrailsFromTrailHead(ref Trail[,] trailMap, (int x, int y) position, int trailId, int count, List<(int x, int y)> previousTrails, bool allowDescent = false) 
+        {
+            previousTrails.Add(position);
+            trailMap[position.x, position.y].accessibleByTrails.Add(trailId);
+            int currentHeight = trailMap[position.x, position.y].height;
+            int oldCount = count;
+
+            if (currentHeight == 9)
+                count++;
+
+            if (position.x != 0 && ValidHeightDifference(currentHeight, trailMap[position.x - 1, position.y].height, allowDescent))
+                count += CountDistinctTrailsFromTrailHead(ref trailMap, (position.x - 1, position.y), trailId, oldCount, previousTrails);
+
+            if (position.x < trailMap.GetLength(0) - 1 && ValidHeightDifference(currentHeight, trailMap[position.x + 1, position.y].height, allowDescent))
+                count += CountDistinctTrailsFromTrailHead(ref trailMap, (position.x + 1, position.y), trailId, oldCount, previousTrails);
+
+            if (position.y != 0 && ValidHeightDifference(currentHeight, trailMap[position.x, position.y - 1].height, allowDescent))
+                count += CountDistinctTrailsFromTrailHead(ref trailMap, (position.x, position.y - 1), trailId, oldCount, previousTrails);
+
+            if (position.y < trailMap.GetLength(1) - 1 && ValidHeightDifference(currentHeight, trailMap[position.x, position.y + 1].height, allowDescent))
+                count += CountDistinctTrailsFromTrailHead(ref trailMap, (position.x, position.y + 1), trailId, oldCount, previousTrails);
+
+            return count;
+        }
+
+        public bool ValidHeightDifference(int heightOne, int heightTwo, bool allowDescent = false) 
+        {
+            if (heightOne == heightTwo && allowDescent) return true;
+            if (heightOne - heightTwo == -1) return true;
+            if (heightOne - heightTwo == 1 && allowDescent) return true;
+            return false;
+        }
+
+        public class Trail 
+        {
+            public int height;
+            public bool trailHead;
+            public int trailHeadId;
+            public List<int> accessibleByTrails;
+
+            public Trail(int height, int trailHeadId) 
+            {
+                this.height = height;
+                trailHead = height == 0;
+                this.trailHeadId = trailHead ?  trailHeadId : -1;
+                accessibleByTrails = new List<int>();
+            
+            }
+        
         }
     }
 }
